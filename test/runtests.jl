018b000a4555bef035ad2e23a0506e999ce4f178
Aaron <akel@umich.edu>
2020-08-05 01:01:02 -0500

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,8 @@ using Test
 
 @testset "ecovivo.jl" begin
     # Write your tests here.
-    ecovivo
+    A = random_communitymatrix(8, 0.1, 0.1)
+    r = random_r_vector(8, 0.1, 0.1)
+
+    assembly_hypergraph(A, r; method = "permanence", regularization = 0)
 end
