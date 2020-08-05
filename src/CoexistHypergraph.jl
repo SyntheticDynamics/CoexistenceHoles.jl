dada4ab16f904836550fdecefb1d4927ec9916e5
Aaron <akel@umich.edu>
2020-08-05 13:08:44 -0500

    change module name in CoexistHypergraph.jl

--- src/CoexistHypergraph.jl
+++ src/CoexistHypergraph.jl
@@ -1,21 +1,21 @@
 
-module ecovivo
+module CoexistHypergraph
 
-        import LinearAlgebra: diagind, rank, pinv, eigvals, diagm
-        import StatsBase: sample
-        import Distributions: Uniform, Normal, Bernoulli, LogNormal
-        import ProgressMeter: @showprogress
-        import Convex: Variable, minimize, solve!
-        import SCS: Optimizer
-        import IterTools: reverse
-        import Ripser: ripser
-        import Combinatorics: combinations
+import LinearAlgebra: diagind, rank, pinv, eigvals, diagm
+import StatsBase: sample
+import Distributions: Uniform, Normal, Bernoulli, LogNormal
+import ProgressMeter: @showprogress
+import Convex: Variable, minimize, solve!
+import SCS: Optimizer
+import IterTools: reverse
+import Ripser: ripser
+import Combinatorics: combinations
 
 
-        include("create_hypergraph.jl")
-        include("hypergraph_analysis.jl")
+include("create_hypergraph.jl")
+include("hypergraph_analysis.jl")
 
-        export random_communitymatrix, random_r_vector, assembly_hypergraph_GLV,
-               disassembly_hypergraph, betti_hypergraph_ripscomplex
+export random_communitymatrix, random_r_vector, assembly_hypergraph_GLV,
+       disassembly_hypergraph, betti_hypergraph_ripscomplex
 
 end
