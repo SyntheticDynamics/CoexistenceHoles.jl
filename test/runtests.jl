1f3fca16face680e56b28e5c72aaa6446ac70f99
Aaron <akel@umich.edu>
2020-08-06 12:06:11 -0500

    new tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using SafeTestsets
 
 @safetestset "Create Growth Vector Tests" begin include("create_growth_vector_tests.jl") end
 @safetestset "Create Community Matrix Tests" begin include("create_community_matrix_tests.jl") end
+@safetestset "GLV Analysis Tests" begin include("GLV_analysis_tests.jl") end
