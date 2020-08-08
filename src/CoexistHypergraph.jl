e6d5f4a2228cd7df5c6c706b49b450f37ef838a6
Aaron <akel@umich.edu>
2020-08-08 17:19:18 -0500

    import Distributions separatly

--- src/CoexistHypergraph.jl
+++ src/CoexistHypergraph.jl
@@ -3,6 +3,7 @@ module CoexistHypergraph
 
 import LinearAlgebra: diagind, rank, pinv, eigvals, diagm, norm, diag
 import StatsBase: sample
+import Distributions # have to import this separately because of a weird thing with JuliaCall in R
 import Distributions: Uniform, Normal, Bernoulli, LogNormal
 import ProgressMeter: @showprogress
 import Convex: Variable, minimize, solve!
